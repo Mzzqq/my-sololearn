051d9805ec80db5f2ffef797d7e12ce6bfc2d04e
Mzzqq <rezarifa661@gmail.com>
2024-02-02 00:39:56 +0700

    add "Sound Frequencies" solution

--- sound-frequencies.go
+++ sound-frequencies.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var f int
+	fmt.Scanln(&f)
+	//your code goes here
+	switch {
+	case f > 20000:
+		fmt.Println("Ultrasound")
+	case f >= 20:
+		fmt.Println("Audible")
+	case f >= 0:
+		fmt.Println("Infrasound")
+	default:
+		fmt.Println("Wrong Input")
+
+	}
 }
