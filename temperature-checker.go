09e0366285c5dae5dfba42ed2caed4e1c180ab63
Mzzqq <rezarifa661@gmail.com>
2024-02-02 00:31:05 +0700

    add "Temperature Checker" solution

--- temperature-checker.go
+++ temperature-checker.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var temperature float32
+	fmt.Scanln(&temperature)
+
+	if temperature > 99.5 {
+		fmt.Println("Fever")
+	} else {
+		fmt.Println("Allowed")
+	}
 }
