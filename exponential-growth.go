2eca0a964bd15bfb5ef6713b3943725c2e1c3732
Mzzqq <rezarifa661@gmail.com>
2024-02-02 00:51:24 +0700

    add "Exponential Growth" solution

--- exponential-growth.go
+++ exponential-growth.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var years int
+	fmt.Scanln(&years)
+
+	//your code goes here
+	result := 7
+	for i := 0; i < years; i++ {
+		result *= 2
+	}
+
+	fmt.Println(result)
 }
