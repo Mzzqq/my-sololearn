a0b131e478ed5c1dad504d96e01a43246b96ba66
Mzzqq <rezarifa661@gmail.com>
2024-02-04 20:55:33 +0700

    add "Age On Mars" solution

--- age-on-mars.go
+++ age-on-mars.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func mars_age(age int) int {
+	return age * 365 / 687
+}
+
+func main() {
+	var age int
+	fmt.Scanln(&age)
+
+	mars := mars_age(age)
+	fmt.Println(mars)
 }
